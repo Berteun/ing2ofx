0c561f87224642ede8d73e9757b3b12dae70c7c9
Berteun Damman <berteun@gmail.com>
2018-01-07 09:59:26 +0100

    Refactor and ability to split by month

--- ing2ofx.py
+++ ing2ofx.py
@@ -4,6 +4,7 @@
 #       ing2ofx.py
 #
 #       Copyright 2013,2016 Arie van Dobben <avandobben@gmail.com>
+#       Copyright 2017, Berteun Damman
 #
 #       This program is free software: you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
@@ -27,6 +28,9 @@ originally found at http://blog.maashoek.nl/2009/07/gnucash-en-internetbankieren
 which is now dead.
 
 The ofx specification can be downloaded from http://www.ofx.net/
+
+ING Specifications can be downloaded from ING:
+https://www.ing.nl/media/266197_1216_tcm162-117728.pdf (may become outdated)
 """
 
 import csv
@@ -34,112 +38,94 @@ import argparse
 import datetime
 import os
 import re
+from collections import defaultdict
 
-""" Read the csv file into a list, which is mapped to ofx fields """
+def ing_code_to_trntype(ing_code, af_bij):
+    af_bij_codes = { 'DV', 'OV', 'VZ' }
+    af_bij_map   = { 'Af'  : 'DEBIT',
+                     'Bij' : 'CREDIT' }
 
+    trn_codes =    { 'GT': 'PAYMENT', 
+                     'BA': 'POS', 
+                     'GM': 'ATM', 
+                     'IC': 'DIRECTDEBIT', 
+                     'ST': 'DIRECTDEP',
+                   }
 
-class CsvFile:
+    if ing_code in af_bij_codes:
+        return af_bij_map[af_bij]
+    else:
+        return trn_codes.get(ing_code, 'OTHER')
 
-    def __init__(self, args):
-        # Mapping of codes to TRNTYPE
-        codesx = {'GT': 'PAYMENT', 'BA': 'POS', 'GM': 'ATM', 'DV': 'xx',
-                  'OV': 'xx', 'VZ': 'xx', 'IC': 'DIRECTDEBIT', 'ST': 'DIRECTDEP'}
-        self.transactions = list()
-        args = args
+def get_transaction_amount(amount, af_bij):
+    """Converts the amount to negative iff *af_bij* is 'Af', and replaces ',' by '.'"""
+    amount = amount.replace(',', '.')
+    sign = '-' if af_bij == 'Af' else ''
+    return sign + amount
+
+def fix_text(ing_text):
+    text = re.sub('  +', ' ', ing_text).strip()
+    return text.replace('&', '&amp;').replace('>', '&gt;').replace('<', '&lt;')
+
+def extract_time(memo):
+    # Extracts time from "Mededelingen" if there's something in there resembling a date time.
+    matches = re.search(r'\d{2}-\d{2}-(?:20)?\d{2} ([0-9]{2}):([0-9]{2})', memo)
+    if matches:
+        return matches.group(1) + matches.group(2)
+    return ''
+
+def make_unique_id(accountto, dtposted, time, trnamt, idslist):
+    # the FITID is composed of the bankaccountto, time, date and amount
+    fitid = accountto + dtposted + time + trnamt.replace("-", "").replace(".", "")
+
+    # Check if we already used a certain ID
+    idcount = 0
+    unique_id = fitid
+    while unique_id in idslist:
+      idcount += 1
+      unique_id = fitid + str(idcount)
+
+    return unique_id
+
+def read_csv_file(filename, split_by_month):
+    transactions = defaultdict(list)
+
+    with open(filename, 'rb') as csvfile:
+        # Open the csvfile as a Dictreader
+        csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
 
         # Keep track of used IDs to prevent double IDs
-        idslist = []
+        idslist = set()
 
-        with open(args.csvfile, 'rb') as csvfile:
-            # Open the csvfile as a Dictreader
-            csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-            for row in csvreader:
-                # Map ACCOUNT to "Rekening"
-                account = row['Rekening'].replace(" ", "")
+        for row in csvreader:
+            # Map ACCOUNT to "Rekening"
+            account = re.sub(r' +', '', row['Rekening'])
+            trntype =  ing_code_to_trntype(row['Code'], row['Af Bij'])
+            dtposted = row['Datum']
+            trnamt = get_transaction_amount(row['Bedrag (EUR)'], row['Af Bij'])
+            name = fix_text(row['Naam / Omschrijving'])
 
-                # Map the code into ofx TRNTYPE
-                if row['Code'] in codesx:
-                    if codesx[row['Code']] == 'xx':
-                        if row['Af Bij'] == 'Bij':
-                            trntype = 'CREDIT'
-                        else:
-                            trntype = 'DEBIT'
-                    else:
-                        trntype = codesx[row['Code']]
-                else:
-                    trntype = 'OTHER'
+            # BANKACCTTO maps to "Tegenrekening"
+            accountto = row['Tegenrekening']
+            # MEMO maps to "Mededelingen"
+            memo = fix_text(row['Mededelingen'])
+            time = extract_time(memo)
 
-                # The DTPOSTED is in yyyymmdd format, which is compatible with ofx
-                # If convert_date is true, first convert dd-mm-yyyy to yyyymmdd
-                if args.convert_date:
-                    datevar = row['Datum'].split('-')
-                    datevar.reverse()
-                    dtposted = ''.join(datevar)
-                else:
-                    dtposted = row['Datum']
+            unique_id = make_unique_id(accountto, dtposted, time, trnamt, idslist)
+            # Append ID to list with IDs
+            idslist.add(unique_id)
 
-                # The TRNAMT needs to be converted to negative if applicable,
-                # When convert is set, comma decimal separator is replaced with
-                # dot.
-                if args.convert:
-                    row['Bedrag (EUR)'] = row['Bedrag (EUR)'].replace(",", ".")
-                if row['Af Bij'] == 'Bij':
-                    trnamt = row['Bedrag (EUR)']
-                else:
-                    trnamt = "-" + row['Bedrag (EUR)']
+            month = dtposted[:6] if split_by_month else ''
 
-                # NAME maps to "Naam / Omschrijving", the while loop removes
-                # any double spaces.
-                while row['Naam / Omschrijving'].strip().find("  ") > 0:
-                    row['Naam / Omschrijving'] = row[
-                        'Naam / Omschrijving'].strip().replace("  ", " ")
-                # Replace & symbol with &amp to make xml compliant
-                name = row['Naam / Omschrijving'].replace("&", "&amp")
+            transactions[month].append(
+                {'account': account, 'trntype': trntype, 'dtposted': dtposted,
+                 'trnamt': trnamt, 'fitid': unique_id, 'name': name, 'accountto': accountto,
+                 'memo': memo})
 
-                # BANKACCTTO maps to "Tegenrekening"
-                accountto = row['Tegenrekening']
-
-                # MEMO maps to "Mededelingen", the while loop removes any
-                # double spaces.
-                while row['Mededelingen'].strip().find("  ") > 0:
-                    row['Mededelingen'] = row[
-                        'Mededelingen'].strip().replace("  ", " ")
-                # Replace & symbol with &amp to make xml compliant
-                memo = str(row['Mededelingen']).replace("&", "&amp")
-
-                # Extract time from "Mededelingen"
-                time = ""
-                matches = re.search("\s([0-9]{2}:[0-9]{2})\s", memo)
-                if matches:
-                  time = matches.group(1).replace(":", "")
-
-                # the FITID is composed of the bankaccountto, time, date and amount
-                fitid = accountto + dtposted + time + \
-                    trnamt.replace(",", "").replace("-", "").replace(".", "")
-
-                # Check if we already used a certain ID
-                idcount = 0
-                uniqueid = fitid
-                while uniqueid in idslist:
-                  idcount = idcount + 1
-                  uniqueid = fitid + str(idcount)
-
-                # Append ID to list with IDs
-                idslist.append(uniqueid)
-
-                self.transactions.append(
-                    {'account': account, 'trntype': trntype, 'dtposted': dtposted,
-                     'trnamt': trnamt, 'fitid': uniqueid, 'name': name, 'accountto': accountto,
-                     'memo': memo})
+    return transactions
 
 
-class OfxWriter:
-
-    def __init__(self, args, gui=True):
-        date = datetime.date.today()
-        nowdate = str(date.strftime("%Y%m%d"))
-        args = args
-        message_header = """
+MESSAGE_HEADER_TEMPLATE = """
 <OFX>
    <SIGNONMSGSRSV1>
       <SONRS>                            <!-- Begin signon -->
@@ -147,10 +133,10 @@ class OfxWriter:
             <CODE>0</CODE>               <!-- OK -->
             <SEVERITY>INFO</SEVERITY>
          </STATUS>
-         <DTSERVER>%(nowdate)s</DTSERVER>   <!-- Oct. 29, 1999, 10:10:03 am -->
+         <DTSERVER>{current_date}</DTSERVER>   <!-- Oct. 29, 1999, 10:10:03 am -->
          <LANGUAGE>ENG</LANGUAGE>        <!-- Language used in response -->
-         <DTPROFUP>%(nowdate)s</DTPROFUP>   <!-- Last update to profile-->
-         <DTACCTUP>%(nowdate)s</DTACCTUP>   <!-- Last account update -->
+         <DTPROFUP>{current_date}</DTPROFUP>   <!-- Last update to profile-->
+         <DTACCTUP>{current_date}</DTACCTUP>   <!-- Last account update -->
          <FI>                            <!-- ID of receiving institution -->
             <ORG>NCH</ORG>               <!-- Name of ID owner -->
             <FID>1001</FID>              <!-- Actual ID -->
@@ -163,117 +149,140 @@ class OfxWriter:
          <STATUS>                     <!-- Start status aggregate -->
             <CODE>0</CODE>            <!-- OK -->
             <SEVERITY>INFO</SEVERITY>
-         </STATUS>""" % {"nowdate": nowdate}
-        # print message_header
+         </STATUS>"""
 
-        # create path to ofxfile
-        if args.outfile:
-            filename = args.outfile.replace("csv", "ofx").replace("CSV", "OFX")
-        else:
-            csvfile = os.path.basename(args.csvfile)
-            filename = csvfile.replace("csv", "ofx").replace("CSV", "OFX")
-
-        # if directory does not exists, create it.
-        if not os.path.exists(args.dir):
-            if not os.path.exists(os.path.join(os.getcwd(), args.dir)):
-                os.makedirs(os.path.join(os.getcwd(), args.dir))
-                args.dir = os.path.join(os.getcwd(), args.dir)
-
-        filepath = os.path.join(args.dir, filename)
-
-        # Initiate a csv object that contains all the data in a set.
-        csv = CsvFile(args)
-
-        # Determine unique accounts and start and end dates
-        accounts = set()
-        mindate = 999999999
-        maxdate = 0
-
-        for trns in csv.transactions:
-            accounts.add(trns['account'])
-            if int(trns['dtposted']) < mindate:
-                mindate = int(trns['dtposted'])
-            if int(trns['dtposted']) > maxdate:
-                maxdate = int(trns['dtposted'])
-
-        # open ofx file, if file exists, gets overwritten
-        with open(filepath, 'w') as ofxfile:
-            ofxfile.write(message_header)
-
-            for account in accounts:
-                message_begin = """
-         <STMTRS>                         <!-- Begin statement response -->
-            <CURDEF>EUR</CURDEF>
-            <BANKACCTFROM>                <!-- Identify the account -->
-               <BANKID>121099999</BANKID> <!-- Routing transit or other FI ID -->
-               <ACCTID>%(account)s</ACCTID> <!-- Account number -->
-               <ACCTTYPE>CHECKING</ACCTTYPE><!-- Account type -->
-            </BANKACCTFROM>               <!-- End of account ID -->
-            <BANKTRANLIST>                <!-- Begin list of statement trans. -->
-               <DTSTART>%(mindate)s</DTSTART>
-               <DTEND>%(maxdate)s</DTEND>""" % {"account": account, "mindate": mindate, "maxdate": maxdate}
-                ofxfile.write(message_begin)
-
-                for trns in csv.transactions:
-                    if trns['account'] == account:
-                        message_transaction = """
-               <STMTTRN>
-                  <TRNTYPE>%(trntype)s</TRNTYPE>
-                  <DTPOSTED>%(dtposted)s</DTPOSTED>
-                  <TRNAMT>%(trnamt)s</TRNAMT>
-                  <FITID>%(fitid)s</FITID>
-                  <NAME>%(name)s</NAME>
-                  <BANKACCTTO>
-                     <BANKID></BANKID>
-                     <ACCTID>%(accountto)s</ACCTID>
-                     <ACCTTYPE>CHECKING</ACCTTYPE>
-                  </BANKACCTTO>
-                  <MEMO>%(memo)s</MEMO>
-               </STMTTRN>""" % trns
-                        ofxfile.write(message_transaction)
-
-                message_end = """
-            </BANKTRANLIST>                   <!-- End list of statement trans. -->
-            <LEDGERBAL>                       <!-- Ledger balance aggregate -->
-               <BALAMT>0</BALAMT>
-               <DTASOF>199910291120</DTASOF><!-- Bal date: 10/29/99, 11:20 am -->
-            </LEDGERBAL>                      <!-- End ledger balance -->
-         </STMTRS>"""
-                ofxfile.write(message_end)
-
-            message_footer = """
+MESSAGE_FOOTER_TEMPLATE = """
       </STMTTRNRS>                        <!-- End of transaction -->
    </BANKMSGSRSV1>
 </OFX>
-      """
-            ofxfile.write(message_footer)
+"""
 
-        if not gui:
-            # print some statistics:
-            print "TRANSACTIONS: " + str(len(csv.transactions))
-            print "IN:           " + args.csvfile
-            print "OUT:          " + filename
-        else:
-            self.stats_transactions = "TRANSACTIONS: " + \
-                str(len(csv.transactions))
-            self.stats_in = "IN:           " + args.csvfile
-            self.stats_out = "OUT:          " + filepath
+MESSAGE_BEGIN_TEMPLATE = """
+     <STMTRS>                         <!-- Begin statement response -->
+        <CURDEF>EUR</CURDEF>
+        <BANKACCTFROM>                <!-- Identify the account -->
+           <BANKID>121099999</BANKID> <!-- Routing transit or other FI ID -->
+           <ACCTID>{account}</ACCTID> <!-- Account number -->
+           <ACCTTYPE>CHECKING</ACCTTYPE><!-- Account type -->
+        </BANKACCTFROM>               <!-- End of account ID -->
+        <BANKTRANLIST>                <!-- Begin list of statement trans. -->
+           <DTSTART>{start_date}</DTSTART>
+           <DTEND>{end_date}</DTEND>""" 
 
-if __name__ == "__main__":
-    """ First parse the command line arguments. """
+MESSAGE_TRANSACTION_TEMPLATE = """
+           <STMTTRN>
+              <TRNTYPE>{trntype}</TRNTYPE>
+              <DTPOSTED>{dtposted}</DTPOSTED>
+              <TRNAMT>{trnamt}</TRNAMT>
+              <FITID>{fitid}</FITID>
+              <NAME>{name}</NAME>
+              <BANKACCTTO>
+                 <BANKID></BANKID>
+                 <ACCTID>{accountto}</ACCTID>
+                 <ACCTTYPE>CHECKING</ACCTTYPE>
+              </BANKACCTTO>
+              <MEMO>{memo}</MEMO>
+           </STMTTRN>""" 
+
+MESSAGE_END_TEMPLATE = """
+        </BANKTRANLIST>                   <!-- End list of statement trans. -->
+        <LEDGERBAL>                       <!-- Ledger balance aggregate -->
+           <BALAMT>0</BALAMT>
+           <DTASOF>199910291120</DTASOF><!-- Bal date: 10/29/99, 11:20 am -->
+        </LEDGERBAL>                      <!-- End ledger balance -->
+     </STMTRS>"""
+
+def open_output(args, month_prefix):
+    # create path to ofxfile
+    if month_prefix:
+        month_prefix = month_prefix + '_'
+
+    if not args.outfile:
+        csvfile = os.path.basename(args.csvfile)
+        filename = month_prefix + csvfile.replace("csv", "ofx").replace("CSV", "OFX")
+    else:
+        filename = month_prefix + args.outfile
+
+    # if directory does not exists, create it.
+    if not os.path.exists(args.dir):
+        if not os.path.exists(os.path.join(os.getcwd(), args.dir)):
+            os.makedirs(os.path.join(os.getcwd(), args.dir))
+            args.dir = os.path.join(os.getcwd(), args.dir)
+
+    return os.path.join(args.dir, filename)
+
+def make_header():
+    today_str = datetime.date.today().strftime('%Y%m%d')
+    return MESSAGE_HEADER_TEMPLATE.format(current_date = today_str)
+
+def make_message_start(account, mindate, maxdate):
+    return MESSAGE_BEGIN_TEMPLATE.format(account=account, start_date=mindate, end_date=maxdate)
+
+def make_transaction(transaction):
+    return MESSAGE_TRANSACTION_TEMPLATE.format(**transaction)
+
+def make_message_end():
+    return MESSAGE_END_TEMPLATE
+
+def make_footer():
+    return MESSAGE_FOOTER_TEMPLATE
+
+def find_accounts(transactions):
+    return { t['account'] for t in transactions }
+
+def find_date_range(transactions):
+    min_date = min(int(t['dtposted']) for t in transactions)
+    max_date = max(int(t['dtposted']) for t in transactions)
+    return min_date, max_date
+
+def write_ofx_file(transactions, ofxfile):
+    # Determine unique accounts and start and end dates
+    accounts = find_accounts(transactions)
+    mindate, maxdate = find_date_range(transactions)
+
+    ofxfile.write(make_header())
+    for account in accounts:
+        ofxfile.write(make_message_start(account, mindate, maxdate))
+
+        account_transactions = (t for t in transactions if t['account'] == account)
+        for trns in account_transactions:
+            ofxfile.write(make_transaction(trns))
+
+        ofxfile.write(make_message_end())
+
+    ofxfile.write(make_footer())
+
+def parse_arguments():
     parser = argparse.ArgumentParser(prog='ing2ofx', description="""
                                      This program converts ING (www.ing.nl) CSV files to OFX format.
                                      The default output filename is the input filename.
                                      """)
+
     parser.add_argument('csvfile', help='A csvfile to process')
+    parser.add_argument('-s, --split', dest='split',
+                        help='Split by month', action='store_true', default=False)
     parser.add_argument('-o, --outfile', dest='outfile',
                         help='Output filename', default=None)
     parser.add_argument('-d, --directory', dest='dir',
                         help='Directory to store output, default is ./ofx', default='ofx')
-    parser.add_argument('-c, --convert', dest='convert',
-                        help="Convert decimal separator to dots (.), default is false", action='store_true')
-    parser.add_argument('-b, --convert_date', dest='convert_date',
-                        help="Convert dates with dd-mm-yyyy notation to yyyymmdd", action='store_true')
-    args = parser.parse_args()
+    return parser.parse_args()
+    
+def main():
+    args = parse_arguments()
+    transactions = read_csv_file(args.csvfile, args.split)
 
-    ofx = OfxWriter(args, gui=False)
+    output_files = []
+    for month in sorted(transactions.keys()):
+        output_file = open_output(args, month)
+        output_files.append(output_file)
+        with open(output_file, 'w') as ofxfile:
+            write_ofx_file(transactions[month], ofxfile)
+
+    # print some statistics:
+    print "Transactions: " + str(sum(len(t) for t in transactions.values()))
+    print "Input:        " + args.csvfile
+    print "Output(s):    " + ",".join(output_files)
+
+
+if __name__ == "__main__":
+    main()
